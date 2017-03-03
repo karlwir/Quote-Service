734fb064cb21c54c0283d7770a1ca37905b9cb72
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-03-03 15:34:42 +0100

    reverted test

--- src/main/java/se/kawi/quoteservice/QuoteServiceApplication.java
+++ src/main/java/se/kawi/quoteservice/QuoteServiceApplication.java
@@ -6,13 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class QuoteServiceApplication extends SpringBootServletInitializer {
-public class QuoteServiceApplication {
+public class QuoteServiceApplication extends SpringBootServletInitializer {
 
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(QuoteServiceApplication.class);
-//    }
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QuoteServiceApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(QuoteServiceApplication.class, args);
