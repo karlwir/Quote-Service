b9b8fbccbc4b45d92e63e4eedd7f272e31281957
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-03-11 18:16:34 +0100

    Added cors for heroku, again

--- src/main/java/se/kawi/quoteservice/resource/CORSFilter.java
+++ src/main/java/se/kawi/quoteservice/resource/CORSFilter.java
@@ -12,7 +12,7 @@ public class CORSFilter implements ContainerResponseFilter {
 
    @Override
    public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext cres) throws IOException {
-      cres.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:8000, https://kw-quoteservice.herokuapp.com");
+      cres.getHeaders().add("Access-Control-Allow-Origin", "https://kw-quoteservice.herokuapp.com");
       cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
       cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
       cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
